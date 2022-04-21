466a038a7366d928ae629557ce51baa131f0b0dd
Purva Vijayvargiya <59122511+casanova5@users.noreply.github.com>
2022-04-21 20:23:12 +0530

    Delete FinalAssignment/shopapp2 directory

--- FinalAssignment/shopapp2/src/main/java/com/shopapp/dao/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.shopapp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductDao extends JpaRepository<Product, Integer>{
-}
